70b0fcc924d152522e88443ee10023ddbd4ea03a
kazurei <167180368+kazurei@users.noreply.github.com>
2024-05-02 08:53:50 +0000

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,7 @@
-
 import streamlit as st
 import random
-
 st.title("おみくじアプリ")
-
-if st.button("回す");
-
-results =("大吉","中吉","小吉」")
-result =ramdom.choice(result)
+if st.button("おみくじを引く")
+results=["大吉","中吉","小吉"]
+result=random.choice(result)
 st.write(f"結果:{result}")
