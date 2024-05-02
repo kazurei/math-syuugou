62498dc76f8cdec1ae4d8f6a5e62e767dfec1520
kazurei <167180368+kazurei@users.noreply.github.com>
2024-05-02 08:46:55 +0000

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,6 @@ st.title("おみくじアプリ")
 
 if st.button("回す");
 
-results=("大吉","中吉","小吉」")
-result=ramdom.choice(result)
+results =("大吉","中吉","小吉」")
+result =ramdom.choice(result)
 st.write(f"結果:{result}")
