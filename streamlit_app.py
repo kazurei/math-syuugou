e6382445e86e2473a67cb9c3ee3d08456f65bb5f
kazurei <167180368+kazurei@users.noreply.github.com>
2024-05-08 07:56:09 +0000

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,13 @@
 import streamlit as st
 
-st.text("adsoijfdsafdsa")
+st.title("bmi")
+st.write("体重を入力してください")
+
+weight = st.number_input("体重を入力してください",min_value=1)
+
+height = st.number_input("身長を入力してください",min_value=1)
+
+BMI = weight/height**2
+
+st.write("BMIは"+str(BMI)+"です")
+
