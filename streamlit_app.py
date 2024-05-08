79e4afb27bcb80682582897ca7c068eccf0c1744
kazurei <167180368+kazurei@users.noreply.github.com>
2024-05-08 08:06:18 +0000

    ok

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,9 @@ import streamlit as st
 st.title("bmi")
 st.write("体重を入力してください")
 
-weight = st.number_input("体重を入力してください",min_value=1)
+weight = st.number_input("体重を入力してください",min_value=1.0)
 
-height = st.number_input("身長を入力してください",min_value=1)
+height = st.number_input("身長を入力してください",min_value=1.0)
 
 BMI = weight/height**2
 
