a5e1e9aed112a42dbdd8a840f3bbca60f3a87ef5
kazurei <167180368+kazurei@users.noreply.github.com>
2024-07-05 08:56:37 +0000

    あ

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import matplotlib.pyplot as plt
-from matplotlib_venn import venn2
+from matplotlib_venn import venn2, venn2_circles
 
 def main():
     st.title("集合を求める")
@@ -10,10 +10,10 @@ def main():
     A = st.text_input("集合Aを入力してください")
     B = st.text_input("集合Bを入力してください")
 
-    # 入力された文字列を空白で分割して集合に変換
-    zentaisyuugouu = set(U.split())
-    syuugoua = set(A.split())
-    syuugoub = set(B.split())
+    # 空の場合は空集合として扱う
+    zentaisyuugouu = set(U.split()) if U else set()
+    syuugoua = set(A.split()) if A else set()
+    syuugoub = set(B.split()) if B else set()
 
     # 集合演算
     katu = syuugoua & syuugoub   # AかつB
@@ -37,8 +37,16 @@ def main():
 
     # ベン図の作成と表示
     plt.figure(figsize=(8, 6))
-    venn2(subsets=(len(syuugoua - syuugoua & syuugoub), len(syuugoub - syuugoua & syuugoub), len(syuugoua & syuugoub)), set_labels=('A', 'B'))
-    plt.title("venn diagram")  # ベン図のタイトルを設定
+    v = venn2(subsets=(len(syuugoua - syuugoua & syuugoub), len(syuugoub - syuugoua & syuugoub), len(syuugoua & syuugoub)), set_labels=('A', 'B'))
+    venn2_circles(subsets=(katu, mataha, a_barkatu))
+    plt.title("ベン図")  # ベン図のタイトルを設定
+    plt.annotate('AかつB', xy=v.get_label_by_id('11').get_position() - np.array([0, 0.05]), xytext=(-70,-70),
+                 ha='center', textcoords='offset points', bbox=dict(boxstyle='round,pad=0.5', fc='gray', alpha=0.1),
+                 arrowprops=dict(arrowstyle='->', connectionstyle='arc3,rad=0.5',color='gray'))
+    plt.annotate('AまたはB', xy=v.get_label_by_id('10').get_position() - np.array([0, 0.05]), xytext=(70,-70),
+                 ha='center', textcoords='offset points', bbox=dict(boxstyle='round,pad=0.5', fc='gray', alpha=0.1),
+                 arrowprops=dict(arrowstyle='->', connectionstyle='arc3,rad=-0.5',color='gray'))
+
     plt.tight_layout()
 
     # ベン図を画像として表示
